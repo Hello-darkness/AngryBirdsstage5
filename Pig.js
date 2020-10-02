5c182d32d205b410d3ca1ec2e9000c6ad0ae12be
Hello-darkness <69919094+Hello-darkness@users.noreply.github.com>
2020-10-02 14:53:28 -0400

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,29 @@
+class Pig extends BaseClass {
+    constructor(x, y) {
+        super(x, y, 50, 50);
+        this.visiblity = 255;
+    this.image = loadImage("sprites/enemy.png");
+  }
+
+    display() {
+    
+
+        if (this.body.speed < 3) {
+
+            super.display();
+
+        }
+        else {
+            push();
+            this.image = loadImage("sprites/Enemy_injured.png");
+            World.remove(world, this.body);
+            this.visiblity = this.visiblity- 5
+            tint(255, this.visiblity);
+            image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+            pop();
+          
+        }
+
+    }
+    
+};
